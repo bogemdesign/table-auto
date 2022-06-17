e80765c6399faefa3edc16c70fe5f3550ff2a7fa
Bogem12 <75928934+bogemdesign@users.noreply.github.com>
2022-06-17 09:42:03 +0700

    Menghilangkan ress dan menambahkan bahan 2

--- main.js
+++ main.js
@@ -1,18 +1,42 @@
+// Titik Pada Angka untuk Rupiah
+
+function titikRupiah(angka) {
+  let reverse = angka.toString().split('').reverse().join(''),
+    ribuan = reverse.match(/\d{1,3}/g);
+  ribuan = ribuan.join('.').split('').reverse().join('');
+  return ribuan;
+}
+
+// hilangkan titik pada angka string to.hasilKeInteger
+function hilangkanTitik(angkaTitik) {
+  let jadiAngkaTanpaTitik = parseFloat(angkaTitik.replaceAll('.', ''))
+  return jadiAngkaTanpaTitik;
+}
+
+
+// Tamabh kolom
 function addkolom (){
 let qty = document.getElementById('qty').value;
 let namaBarang = document.getElementById('nama-barang').value;
 let detail = document.getElementById('spesifikasi').value;
 let satuan = document.getElementById('satuan').value
-   
+ let bahan = document.getElementById('bahan').value
 let tabel =document.getElementById('barang');
 
 let total= parseInt(qty)*parseInt(satuan);
+
+// ubah pake titik
+let tqty= titikRupiah(qty)
+let tsatuan = titikRupiah(satuan)
+let  tTotal = titikRupiah(total)
+
 let tema = `<tr>
-        <td>${qty}</td>
+        <td>${tqty}</td>
         <td>${namaBarang}</td>
+        <td>${bahan}</td>
         <td>${detail}</td>
-        <td>${satuan}</td>
-        <td class='total'>${total}</td>
+        <td>${tsatuan}</td>
+        <td class='total'>${tTotal}</td>
             </tr>`
  
  let kolomBaru = tabel.insertRow(1)
@@ -34,23 +58,24 @@ let u=''
   for (i=0 ; i < total.length;i++){
     
     let l=total[i].innerText+";"
-   // console.log(l)
-    u += l
+   console.log(hilangkanTitik(l))
+   let tanpaTitik= hilangkanTitik(l)
+    u += l.replaceAll('.','')
 
     
   }
-  //menghapus titik koma pemisah
+  //menghapus titik koma pemisah Array
   let re = /\s*;\s*/;
   let nameList = u.split(re);
-  
+  console.log(nameList)
  let hasilKeInteger= nameList.map(Number)
-
+console.log(hasilKeInteger)
   let sum = hasilKeInteger.reduce((x, y) => {
     return x + y;
   });
   alert('Data Berhasil Di Jumlahkan')
   
- return document.querySelector('.hasil').innerText=sum;
+ return document.querySelector('.hasil').innerText=titikRupiah(sum);
 
 };
 
@@ -60,30 +85,41 @@ let u=''
 function depo(){
   
   let inputDepo=document.getElementById('deposit').value;
-  let inputRess = document.getElementById('res').value
+ 
+ 
   
-  let ress =document.querySelector('.res')
+  // let ress =document.querySelector('.res')
   let depo =document.querySelector('.depo')
   
-  let isiDepo = depo.textContent=inputDepo;
-  let isiRess= ress.textContent=inputRess;
+  let isiDepo = depo.textContent=titikRupiah(inputDepo);
+  // let isiRess= ress.textContent=titikRupiah(inputRess);
+  let isideponotitik = hilangkanTitik(isiDepo)
   
-  let isideporess= parseInt(isiDepo)+parseInt(isiRess)
+  // let isideporess= hilangkanTitik(isiDepo)+hilangkanTitik(isiRess);
   
   let hasil = document.querySelector('.hasil').textContent;
 
-  let hasilDiKurangiDepo = parseInt(hasil)-parseInt(isideporess);
+  let hasilDiKurangiDepo = hilangkanTitik(hasil)-parseInt(isideponotitik);
+   let namaPelanggan =document.getElementById('nama-pelanggan').value
+  
+  document.querySelector('.nama').textContent=namaPelanggan
+  
+  
   
   let bayar= document.querySelector('.dibayar');
+  
+  
   alert('Data Berhasil di tambahkan')
   
- return bayar.textContent=hasilDiKurangiDepo;
-  
+ return bayar.textContent=titikRupiah(hasilDiKurangiDepo);
   
 
   
 }
 
+
+
+// Tanggal Otomatis
 let namaBulan= ['Januari','Febuari','Maret','April','Mei','Juni','Juli','Agustus','September','Oktober','November','Desember']
 let namaHari =['Minggu','Senin','Selasa','Rabu','Kamis','Jum\'at','Sabtu']
 
